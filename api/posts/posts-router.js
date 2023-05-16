62c1ed8c5712becb7d5165fa0b59d0306dbba828
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-17 02:00:17 +0300

    testler geçildi - proje tamamlandı.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -44,4 +44,54 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await postsModel.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      let { title, contents } = req.body;
+      if (!title || !contents) {
+        res.status(400).json({
+          message: "Lütfen gönderi için bir title ve contents sağlayın",
+        });
+      } else {
+        await postsModel.update(req.params.id, { title, contents });
+        const updatedPost = await postsModel.findById(req.params.id);
+        res.json(updatedPost);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Gönderi bilgileri güncellenemedi" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await postsModel.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      await postsModel.remove(req.params.id);
+      res.json(post);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Gönderi silinemedi" });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const post = await postsModel.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      const postsComments = await postsModel.findPostComments(req.params.id);
+      res.json(postsComments);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Yorumlar bilgisi getirilemedi" });
+  }
+});
+
 module.exports = router;
