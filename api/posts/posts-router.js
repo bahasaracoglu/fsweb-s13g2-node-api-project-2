eb8803f4963df0e5f2ee50602f0c8a29cc5c51d5
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-16 23:44:47 +0300

    get requests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,28 @@
 // posts için gerekli routerları buraya yazın
+const router = require("express").Router();
+const postsModel = require("./posts-model");
+
+router.get("/", async (req, res) => {
+  try {
+    const all = await postsModel.find();
+    res.json(all);
+  } catch (error) {
+    res.status(500).json({ message: "Gönderiler alınamadı" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await postsModel.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    }
+    {
+      res.json(post);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Gönderi bilgisi alınamadı" });
+  }
+});
+
+module.exports = router;
